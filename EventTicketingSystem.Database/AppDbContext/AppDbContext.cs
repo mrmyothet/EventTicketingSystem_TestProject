7238e467c2dc16638d51ccc7a609dbea450d06a7
MyoThet <mr.myothet@gmail.com>
2025-07-10 00:32:30 +0700

    Add Database project

---
+++ EventTicketingSystem.Database/AppDbContext/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTicketingSystem.Database.AppDbContext;
+
+public partial class AppDbContext: DbContext
+{
+    public AppDbContext()
+    {
+        
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+}
