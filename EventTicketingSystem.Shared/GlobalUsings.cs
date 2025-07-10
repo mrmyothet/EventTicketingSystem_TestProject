925afad67513a50ec47fed10c491ace13ef7c43d
MyoThet <mr.myothet@gmail.com>
2025-07-10 18:52:58 +0700

    Edit Shared project

---
+++ EventTicketingSystem.Shared/GlobalUsings.cs
@@ -0,0 +1,3 @@
+﻿global using Newtonsoft.Json;
+global using Microsoft.Extensions.Logging;
+global using System.Runtime.CompilerServices;
---
+++ EventTicketingSystem.Shared/Result.cs
@@ -0,0 +1,46 @@
+﻿
+
+namespace EventTicketingSystem.Shared;
+
+public partial class Result<T>
+{
+    public bool IsSuccess { get; set; }
+    public bool IsError => !IsSuccess;
+    public string Message { get; set; } = string.Empty!;
+    public T Data { get; set; } = default!;
+
+    public static Result<T> Success(T data, string message = "Success!")
+    {
+        return new Result<T> { IsSuccess = true, Data = data, Message = message };
+    }
+
+    public static Result<T> Success(string message = "Success!")
+    {
+        return Success(default!, message);
+    }
+
+    public static Result<T> ValidationError(string message, T? data = default)
+    {
+        return new Result<T> { IsSuccess = false, Data = data, Message = message };
+    }
+
+    public static Result<T> SystemError(string message = "Something went wrong!", T? data = default)
+    {
+        return new Result<T> { IsSuccess = false, Data = data, Message = message };
+    }
+
+    public static Result<T> DuplicateRecordError(string message = "There are Duplicated Records!", T? data = default)
+    {
+        return new Result<T> { IsSuccess = false, Data = data, Message = message };
+    }
+
+    public static Result<T> NotFoundError(string message = "No Data Found!", T? data = default)
+    {
+        return new Result<T> { IsSuccess = false, Data = data, Message = message };
+    }
+
+    public static Result<T> UserInputError(string message = "Wrong User Input!", T? data = default)
+    {
+        return new Result<T> { IsSuccess = false, Data = data, Message = message };
+    }
+}
