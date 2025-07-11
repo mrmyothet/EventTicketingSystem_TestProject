005bc421f24818e87b91af597bd7dcc0303693d1
MyoThet <mr.myothet@gmail.com>
2025-07-11 18:03:09 +0700

    move folders as the same level of solution file

---
+++ EventTicketingSystem.Domain/Features/TicketType/DA_TicketType.cs
@@ -0,0 +1,17 @@
+﻿using EventTicketingSystem.Domain.Features.EventCategory;
+
+namespace EventTicketingSystem.Domain.Features.TicketType;
+
+public class DA_TicketType
+{
+    private readonly ILogger<DA_TicketType> _logger;
+
+    private readonly AppDbContext _db;
+
+    public DA_TicketType(ILogger<DA_TicketType> logger, AppDbContext db = null)
+    {
+        _logger = logger;
+        _db = db;
+    }
+
+}
---
+++ EventTicketingSystem.Domain/Models/Features/TicketType/TicketTypeModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EventTicketingSystem.Domain.Models.Features.TicketType;
+
+public class TicketTypeModel
+{
+    public string Tickettypeid { get; set; }
+
+    public string Tickettypecode { get; set; }
+
+    public string Tickettypename { get; set; }
+}
