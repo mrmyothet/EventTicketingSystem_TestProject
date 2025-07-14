c9bb058778cff692fd6f41bf3d77e1802dd1b265
MyoThet <mr.myothet@gmail.com>
2025-07-14 15:31:21 +0700

    Generate QRCode using BitmapRenderer

--- EventTicketingSystem.Domain/Features/QR/BL_QrCode.cs
+++ EventTicketingSystem.Domain/Features/QR/BL_QrCode.cs
@@ -1,8 +1,18 @@
-﻿namespace EventTicketingSystem.Domain.Features.QR;
+﻿using Microsoft.VisualBasic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using ZXing;
+using ZXing.Common;
+using ZXing.Windows.Compatibility;
+
+namespace EventTicketingSystem.Domain.Features.QR;
 
 public class BL_QrCode
 {
     private readonly DA_QrCode _da_QrCode;
+
+    private const BarcodeFormat DEFAULT_BARCODE_FORMAT = BarcodeFormat.QR_CODE;
+    private static readonly ImageFormat DEFAULT_IMAGE_FORMAT = ImageFormat.Png;
 
     public BL_QrCode(DA_QrCode da_QrCode)
     {
@@ -19,6 +29,25 @@ public class BL_QrCode
         }
 
         response.Message = "QR code generated successfully.";
+
+        string outputFileName = requestModel.TicketCode + "_" + requestModel.Email + ".png";
+
+        // Generate the QR code image using ZXing
+        var writer = new BarcodeWriter<Bitmap>
+        {
+            Format = DEFAULT_BARCODE_FORMAT,
+            Options = new EncodingOptions
+            {
+                Width = 300,
+                Height = 300
+            }, 
+            Renderer = new BitmapRenderer()
+        };
+
+
+        var bitmap = writer.Write(response.Data.QrString);
+
+        bitmap.Save(outputFileName, DEFAULT_IMAGE_FORMAT);
 
         return response;
     }
