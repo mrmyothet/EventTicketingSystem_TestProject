f9283459e9813e320f0d91113513732db5f277d0
MyoThet <mr.myothet@gmail.com>
2025-07-14 12:33:46 +0700

    Add MVC Web App

---
+++ EventTicketingSystem.MVCWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EventTicketingSystem.MVCWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
